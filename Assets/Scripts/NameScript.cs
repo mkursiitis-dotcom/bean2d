a7c030ece42755a59d5718500458041c2368b07a
unknown <mkursiitis@gmail.com>
2026-02-08 20:14:19 +0200

    Pirmas nodarbibas beigas

--- Assets/Scripts/NameScript.cs
+++ Assets/Scripts/NameScript.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NameScript : MonoBehaviour
 {
@@ -17,6 +18,18 @@ public class NameScript : MonoBehaviour
         randomIx = Random.Range(0, sampleText.Length);
         text = inputField.GetComponent<TMP_InputField>().text;
         outputField.GetComponent<TMP_Text>().text = sampleText[randomIx] + " " + text.ToUpper() + "!";
+        reverseTextToggle.GetComponent<Toggle>().interactable = true;
+        if(reverseTextToggle.GetComponent<Toggle>().isOn)
+        {
+            ReverseText();
+        }
+    }
+
+    public void ReverseText()
+    {
+        char[] charArray = outputField.GetComponent<TMP_Text>().text.ToCharArray();
+        System.Array.Reverse(charArray);
+        outputField.GetComponent<TMP_Text>().text = new string(charArray);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
